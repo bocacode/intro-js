8d1aa564c03e0a7be0c892496ac790d67ae24d32
Todd Albert <todd@bocacode.com>
2021-09-23 15:35:21 -0400

    other amazing function added

--- merge.js
+++ merge.js
@@ -1,6 +1,15 @@
 console.log('This file does exist in the main branch.')
 
+const myName = 'Todd #2'
+
 const a = 1
 const b = 2
+const c = 3
 
-console.log(a + b)
+console.log(a + b + c)
+
+function otherAmazingFunction(fizz) {
+  console.log(fizz, fizz, fizz)
+}
+
+otherAmazingFunction('PLOP!')
